f2aa1d5e47c1447540df6f16e402952532940fd6
vyasakku <vyasakku1@gmail.com>
2025-02-08 20:00:27 +0530

    bugs in pagination

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,8 @@
-import { Row, Col } from 'react-bootstrap';
+import React, { useEffect } from 'react'; 
 import { useParams } from 'react-router-dom';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 import { Link } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
@@ -10,28 +11,25 @@ import ProductCarousel from '../components/ProductCarousel';
 import Meta from '../components/Meta';
 
 const HomeScreen = () => {
-  const { pageNumber, keyword } = useParams();
+  const { pageNumber = '1', keyword = '' } = useParams();
+  const pageNum = Number(pageNumber);
 
-  const { data, isLoading, error } = useGetProductsQuery({
-    keyword,
-    pageNumber,
-  });
+  const { data, isLoading, error, refetch } = useGetProductsQuery(
+    { keyword, pageNumber: pageNum },
+    { refetchOnMountOrArgChange: true }
+  );
+
+  useEffect(() => {
+    refetch();
+  }, [pageNum, keyword, refetch]);
 
   return (
     <>
-      {!keyword ? (
-        <ProductCarousel />
-      ) : (
-        <Link to='/' className='btn btn-light mb-4'>
-          Go Back
-        </Link>
-      )}
+      {!keyword ? <ProductCarousel /> : <Link to='/' className='btn btn-light mb-4'>Go Back</Link>}
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant='danger'>
-          {error?.data?.message || error.error}
-        </Message>
+        <Message variant='danger'>{error?.data?.message || error.error}</Message>
       ) : (
         <>
           <Meta />
@@ -43,11 +41,7 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
-          <Paginate
-            pages={data.pages}
-            page={data.page}
-            keyword={keyword ? keyword : ''}
-          />
+          <Paginate pages={data.pages} page={data.page} keyword={keyword} />
         </>
       )}
     </>
--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,12 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../constants';
 
-import { logout } from './authSlice'; // Import the logout action
-
-// NOTE: code here has changed to handle when our JWT and Cookie expire.
-// We need to customize the baseQuery to be able to intercept any 401 responses
-// and log the user out
-// https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#customizing-queries-with-basequery
+import { logout } from './authSlice'; 
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
@@ -22,7 +17,7 @@ async function baseQueryWithAuth(args, api, extra) {
 }
 
 export const apiSlice = createApi({
-  baseQuery: baseQueryWithAuth, // Use the customized baseQuery
+  baseQuery: baseQueryWithAuth, 
   tagTypes: ['Product', 'Order', 'User'],
   endpoints: (builder) => ({}),
 });
